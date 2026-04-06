81fbc3777339d82c06012ca9cd63f81154a08399
Tran Le Trung Dung <tranletrungdung123123@gmail.com>
2026-04-07 00:21:36 +0700

    Thêm nền JavaFX shell tuần 1

---
+++ src/main/java/com/auction/client/controller/MainLayoutController.java
@@ -0,0 +1,8 @@
+package com.auction.client.controller;
+
+public class MainLayoutController {
+
+    public void initialize() {
+        System.out.println("MainLayout loaded successfully.");
+    }
+}
