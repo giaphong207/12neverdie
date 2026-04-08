74e56228a88a87affa43a3dfc29b2c79a0cae8f5
giaphong207 <25021930@vnu.edu.vn>
2026-04-08 11:04:38 +0700

    Hoan thanh Skeleton code va thiet lap CI/CD

---
+++ auction-app/src/main/java/com/auction/shared/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.auction.shared.config;
+
+public final class AppConfig {
+    public static final String DATA_FILE = "data/database.dat";
+    public static final int SERVER_PORT = 9999;
+    private AppConfig() {}
+}
---
+++ auction-app/src/main/java/com/auction/shared/model/User.java
@@ -0,0 +1,5 @@
+package com.auction.shared.model;
+
+public class User {
+    //Để trống trong tuần 1
+}
